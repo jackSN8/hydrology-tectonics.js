91d684f4988743a24b71a24c92d35cef191174d5
Jack Kingdon <jack.id.kingdon@gmail.com>
2024-07-14 22:34:48 -0700

    Added basic JS HTML framework

---
+++ water_2/main.js
@@ -0,0 +1,17 @@
+let cells = [][];
+let resolution = 10;
+let rowLength = 40;
+
+function setup()
+{
+  createCanvas(resolution*rowLength,resolution*rowLength);
+
+  textSize(30);
+  textAlign(CENTER);
+  frameRate(15);
+}
+
+function draw()
+{
+  background(127);
+}
